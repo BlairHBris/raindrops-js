73ef4bc88992e628b79bcbffedd214208f6b59c7
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-21 12:46:19 -0600

    Add solution

---
+++ raindrops.js
@@ -0,0 +1,13 @@
+export function convert(drops) {
+    let converted = "";
+    if (drops % 3 === 0) {
+        converted += "Pling";
+    }
+    if (drops % 5 === 0) {
+        converted += "Plang";
+    }
+    if (drops % 7 === 0) {
+        converted += "Plong";
+    }
+    return converted ? converted : drops.toString();
+}
