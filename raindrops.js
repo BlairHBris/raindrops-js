3b994e4dbb6f7c4852c6a8c832ede8f0fc29753a
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 16:28:02 -0400

    Add solution

--- raindrops.js
+++ raindrops.js
@@ -1,1 +1,16 @@
-export function convert() {}
+export function convert(drops) {
+    let converted = "";
+
+    if (drops % 3 === 0) {
+        converted += "Pling";
+    }
+    if (drops % 5 === 0) {
+        converted += "Plang";
+    }
+    if (drops % 7 === 0) {
+        converted += "Plong";
+    }
+
+    return converted ? converted : drops.toString();
+
+}
